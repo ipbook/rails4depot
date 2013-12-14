a2245b87fa36ba77d2c592bc80c9721a600dbb94
ipbook <ipartbook@hotmail.com>
2013-12-14 13:08:19 +0000

    init Product scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = 'f3451b0e30cb8c70497541e3434a4ae92f7cbc994914a1de7be3e84ba7cb4a03ee36ee4c2d661192d89dec2dcbb77bfa9f47226cf2b4ffc18086448edcccf28b'
