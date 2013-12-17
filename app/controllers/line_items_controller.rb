d62c9721c08c54d1888cb2760c2cf66cd56f6236
ipbook <ipartbook@hotmail.com>
2013-12-17 11:05:51 +0000

    AJAX for     Add to Cart     button

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -37,7 +37,7 @@ class LineItemsController < ApplicationController
         #format.html { redirect_to @line_item.cart  #remove......, notice: 'Line item was successfully created.' 
          # }
         format.html { redirect_to store_url }
-        
+        format.js
         format.json { render action: 'show', 
           status: :created, location: @line_item }
       else
