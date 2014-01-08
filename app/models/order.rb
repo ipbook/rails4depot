9ca485a734b9dbbc39fe6683beaa0283cef5878b
ipbook <ipartbook@hotmail.com>
2014-01-08 10:48:56 +0000

    add validates in models/Order.rb + modify *yml files

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ActiveRecord::Base
   NAME_SIZE = 41
   PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order" ]
+  
+  validates :name, :address, :email, presence: true
+  validates :pay_type, inclusion: PAYMENT_TYPES
 end
