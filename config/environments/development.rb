5a3edbe3f4e586b488472f41cf52f14ca27771a4
ipbook <ipartbook@hotmail.com>
2014-01-09 09:50:15 +0000

    ready mail config AND rails g mailer OrderNotifier received shipped

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,16 @@
 Depot::Application.configure do
+  #config.action_mailer.delivery_method = :test #20140109 if you don't want send mail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { #20140109 rails g mailer OrderNotifier received shipped
+    address: "smtp.gmail.com" ,
+    port: 587,
+    domain: "domain.of.sender.net",
+    authentication: "plain",
+    user_name: "dave" ,
+    password: "secret" ,
+    enable_starttls_auto: true
+  }
+  
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
